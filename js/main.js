9554772d5bd090e78f77b5cb633a64ec1c658fca
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-11-15 15:26:36 -0500

    Updated index and js code

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ $(document).ready(function(){
 	}); 
 });
 
+/*how to code elements to move */ 
+var position = 0;
+var current_card = document.getElementById("card1"); 
+var interval = setInterval(function() {
+	position -= 1;
+	$("body").css({ "card1":+ position +"px 0px" })
+}, 35);
+
+
+
+
+/*
+function AnimateCard(){
+	var 
+}
+*/
